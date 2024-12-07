1e187bf94a48bccc3ad51a4dee93e7aec23b6f97
matteogaillard <matteo.gaillard@protonmail.com>
2024-12-07 14:23:40 +0100

    [update] pago-flash showcase (without responsive)

--- src/components/PagoFlash.vue
+++ src/components/PagoFlash.vue
@@ -4,7 +4,7 @@
   </div>
 
   <div class="hero-header">
-    <img alt="pago-flash" src="../assets/images/pago-flash/pago-flash.svg" />
+    <img alt="pago-flash" src="/images/pago-flash/pago-flash.svg" />
     <h1>Pago Flash</h1>
     <h2>Le flash éphémère pour tous vos évènements</h2>
   </div>
@@ -19,13 +19,11 @@
             viennent des meilleures coins du monde.</p>
         </div>
         <div>
-          <h2>Un savoir-faire</h2>
-          <p>Les pago-flash sont préparés avec amour dans les meilleurs
-            quartier de Lannion. La <b>vodka</b>, le <b>rhum</b>, le <b>whisky</b> et le <b>ricard</b>
-            viennent des meilleures coins du monde.</p>
+          <h2>Des partenaires d'exceptions</h2>
+          <p>Nos partenaires fournissent les meilleurs flash de toute la région. Nasdas, Samos, Assia, mettent un point d'honneur à fournir les meilleurs ballons pour aller avec le bon flash, comme du vin quoi.</p>
         </div>
       </div>
-      <img src="../assets/images/pago-flash/savoir-faire.jpg" alt="Pago flash savoir faire" />
+      <img src="/images/pago-flash/savoir-faire.jpg" alt="Pago flash savoir faire" />
     </article>
 
     <article class="partenaires">
@@ -33,23 +31,25 @@
 
       <ul>
         <li>
-          <img src="../assets/images/pago-flash/partenaires/momoplansnap.jpg" alt="momoplansnap" />
+          <img src="/images/pago-flash/partenaires/momoplansnap.jpg" alt="momoplansnap" />
         </li>
         <li>
-          <img src="../assets/images/pago-flash/partenaires/ballon2zipette.jpg" alt="ballon2zipette" />
+          <img src="/images/pago-flash/partenaires/ballon2zipette.jpg" alt="ballon2zipette" />
         </li>
         <li>
-          <img src="../assets/images/pago-flash/partenaires/planBraquage.jpg" alt="planBraquage" />
+          <img src="/images/pago-flash/partenaires/planBraquage.jpg" alt="planBraquage" />
         </li>
       </ul>
     </article>
 
     <article class="selection">
-      <h2>Notre selection</h2>
+      <h2>Notre offres</h2>
 
       <section>
-        <article>
-
+        <article v-for="offer in offers" :key="offer">
+          <h4 v-if="offer.popular">Populaire</h4>
+          <img :src="'images/pago-flash/thumbnails/'+offer.thumbnail+'.png'" :alt="offer.title" />
+          <h3>{{ offer.title }}</h3>
         </article>
       </section>
     </article>
@@ -57,7 +57,27 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const offers = ref([
+  {
+    thumbnail: "pack-1",
+    title: "VodKH + 3 ballons OFFERT",
+    popular: true
+  },
+  {
+    thumbnail: "pack-2",
+    title: "Whisky-flash + formation CRYPTO"
+  },
+  {
+    thumbnail: "vodka",
+    title: "VodKH"
+  },
+  {
+    thumbnail: "whisky",
+    title: "Whisky-flash"
+  }
+]);
 </script>
 
 <style scoped>
@@ -126,6 +146,7 @@
         display: flex;
         justify-content: center;
         gap: 2em;
+        flex-wrap: wrap;
         list-style: none;
 
         &>li {
@@ -143,6 +164,49 @@
       &>h2 {
         text-align: center;
         margin-bottom: 1em;
+      }
+
+      &>section {
+        display: flex;
+        gap: 1em;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin: 3em 0;
+
+        &>article {
+          background-color: var(--black-2);
+          flex-basis: 10em;
+
+          border-radius: 15px;
+          padding: 1em 2em;
+
+          text-align: center;
+          position: relative;
+
+          &>h4 {
+            position: absolute;
+            top: -23px;
+            left: 50%;
+
+            transform: translateX(-50%);
+
+            background-color: var(--orange);
+            color: var(--black);
+
+            padding: .5em .8em;
+            border-radius: 100px;
+          }
+
+          &>img {
+            width: 15em;
+            height: 15em;
+            margin-bottom: .5em;
+          }
+
+          &>h3 {
+            font-family: "poppins-bold", sans-serif;
+          }
+        }
       }
     }
   }
