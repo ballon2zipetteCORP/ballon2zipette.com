ff24e3bf42ef6f4469abeb02bbdbda05c85085a1
Mattéo G. <40558258+Matteo0810@users.noreply.github.com>
2024-12-10 08:28:50 +0100

    [FIX] build base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ballon2zipette.com/",
+  //base: "/ballon2zipette.com/",
   plugins: [
     vue(),
     vueDevTools(),
