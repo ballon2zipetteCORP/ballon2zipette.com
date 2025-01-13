0ff25204ccfc18b075b8d88075513b07bd133611
Mattéo G. <40558258+Matteo0810@users.noreply.github.com>
2025-01-13 20:22:43 +0100

    Update OurTeam.vue

--- src/components/OurTeam.vue
+++ src/components/OurTeam.vue
@@ -33,7 +33,7 @@ import CardCalibre from '@/components/ui/cards/CardDefault.vue'
 const OUR_TEAM = ref([
   { thumbnail: "ballon2zipette.jpg", post: "CEO", video: ["ballon2zipette.mov", "ballon2zipette-2.mp4"] },
   { thumbnail: "momoplansnap.jpg", post: "CEO", video: "momoplansnap.mp4" },
-  { thumbnail: "planBraquageMechant.jpg", post: "CEO", video: ["plan-braquage.mov", "plan-braquage-2.mp4"]}, //, boycottRate: 2, boycottReason: "Vente d'arme 🔫"
+  { thumbnail: "planBraquageMechant.jpg", post: "CEO", video: ["plan-braquage.mov", "plan-braquage-2.mp4"], boycottRate: 2, boycottReason: "Il casse les couilles"},
   { thumbnail: "panoramix.jpg", post: "Associé.e", video: "panoramix.mp4" },
   { thumbnail: "nourisse.jpg", post: "Associé.e" },
   { thumbnail: "panorapetite.jpg", post: "Associé.e", video: "panorapetite.mp4" }
