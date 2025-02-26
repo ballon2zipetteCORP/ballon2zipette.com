d69fbc04530372e21cde7ee9864118b00f0044c5
matteogaillard <matteo.gaillard@protonmail.com>
2025-02-26 13:38:33 +0100

    updated homepage

--- src/components/events/list/TravauxALaChiche.vue
+++ src/components/events/list/TravauxALaChiche.vue
@@ -4,9 +4,12 @@
         <h1>Travaux à la chiche</h1>
     </div>
     <div class="new">
-        <h2>Rosé à volonté !</h2>
-        <h3>Exclusivement pour cette chiche, nous avons prévus 2 cubi de rosé bien BTP bien 8h-16h intérim.</h3>
-        <img alt="hero-header image" src="/images/events/travaux-a-la-chiche/dumb.jpg" />
+        <h2>
+            <span class="new-tag">Nouveauté</span>
+            Rosé à volonté !
+        </h2>
+        <h3>Exclusivement pour cette chiche, nous avons prévus du rosé bien BTP bien 8h-16h intérim.</h3>
+        <img alt="hero-header image" src="/images/events/travaux-a-la-chiche/cubi.jpg" />
     </div>
 </template>
 
@@ -39,7 +42,26 @@ div.new {
         width: fit-content;
         padding: .5em .8em;
         margin: auto;
+
+        margin-top: 2em;
         margin-bottom: .5em;
+
+        position: relative;
+
+        &>span.new-tag {
+            background-color: var(--red);
+            color: white;
+            padding: .5em .8em;
+            border-radius: 10px;
+
+            font-size: .6em;
+
+            position: absolute;
+            top: -1.5em;
+            right: -1.5em;
+
+            transform: rotate(20deg);
+        }
     }
 
     &>img {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <template>
-  <!--<travaux-a-la-chiche />-->
-  <waiting-screen />
+  <travaux-a-la-chiche />
 </template>
 
 <script setup>
 import TravauxALaChiche from '@/components/events/list/TravauxALaChiche.vue';
-import WaitingScreen from '@/components/WaitingScreen.vue';
 </script>
