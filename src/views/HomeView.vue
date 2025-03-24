11259d57d4303909f33b962a60e6e97c8fc5c07b
bluntwise <alandely11@gmail.com>
2025-03-24 20:01:55 +0100

    [FIX] homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <!--<WackyParty />-->
-  <WaitingScreen />
+<!--  <WaitingScreen />-->
+  <krakenssat></krakenssat>
 </template>
 
 <script setup>
 import WackyParty from '@/components/events/list/WackyParty.vue';
 import WaitingScreen from '@/components/WaitingScreen.vue';
+import Krakenssat from '@/components/events/list/krakenssat.vue'
 </script>
